5d062ac9556e125c6622767574c45c6b4b028d1d
Michał Olekszy <michal.olekszy@gmail.com>
2019-03-31 17:09:27 +0200

    Add image to patient

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -75,7 +75,7 @@ end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def patient_params
-      params.require(:patient).permit(:name, :surname, :pesel, address_attributes: [:city, :street_name, :street_number, :postal_code, :id])
+      params.require(:patient).permit(:name, :surname, :pesel, :avatar, address_attributes: [:city, :street_name, :street_number, :postal_code, :id])
 
     end
 end
