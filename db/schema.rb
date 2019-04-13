09e249847b859675bf60246382c25d5caf434565
Michał Olekszy <michal.olekszy@gmail.com>
2019-04-13 16:54:30 +0200

    Add styles to Patients + reset db

--- db/schema.rb
+++ db/schema.rb
@@ -104,15 +104,6 @@ ActiveRecord::Schema.define(version: 2019_03_31_145624) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "stuffs", force: :cascade do |t|
-    t.string "name"
-    t.string "surname"
-    t.string "pesel"
-    t.integer "type", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "appointments", "reservations"
   add_foreign_key "bills", "appointments"
