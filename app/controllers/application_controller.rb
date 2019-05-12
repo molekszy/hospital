d0f482010bc659a3681e477d098a9b89d5cf8544
Michał Olekszy <michal.olekszy@gmail.com>
2019-05-12 10:02:57 +0200

    Add PL language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   helper_method :authenticate_admin
    before_action :configure_permitted_parameters, if: :devise_controller?
+   before_action :set_locale
 
 def authenticate_admin
   return if current_user.admin?
@@ -11,5 +12,9 @@ end
 
  def configure_permitted_parameters
    devise_parameter_sanitizer.permit(:account_update, keys: [:pesel, :birth_date, :avatar ])
+ end
+
+ def set_locale
+   I18n.locale = params[:locale] || I18n.default_locale
  end
 end
